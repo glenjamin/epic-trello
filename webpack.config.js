47de1ca2b454cfb022f0737ebd3a2c6c6755c759
Glen Mailer <glen@stainlessed.co.uk>
2018-05-13 20:55:33 +0100

    Add simple error/warning overlay

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,10 @@ const path = p => require("path").resolve(__dirname, p);
 module.exports = {
   mode,
   context: __dirname,
-  entry: ["./client"],
+  entry: [
+    "./client",
+    mode === "development" && "@glenjamin/webpack-hot-client-overlay"
+  ].filter(Boolean),
   output: {
     publicPath: "/",
     path: path("./dist"),
